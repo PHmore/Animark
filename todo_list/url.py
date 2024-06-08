430a347512143ce86151f65ccfffb2e7ac5709c1
PHmore <patryckhenryck401@gmail.com>
2024-06-07 22:44:10 -0300

    Inicio da estilização e RFs do projeto

--- todo_list/url.py
+++ todo_list/url.py
@@ -2,11 +2,3 @@ from django.contrib import admin
 from django.urls import path,include
 from todo_list.views import *
 
-
-
-urlpatterns = [
-    path('',Login.as_view(),name='index'),
-    path('logout/',Logout.as_view(),name='logout'),
-    path('cadastro/',Cadastro.as_view(),name='cadastro'),
-    path('cadastro/',Cadastro.as_view(),name='cadastro'),
-]
